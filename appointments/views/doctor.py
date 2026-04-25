d6110fad8d9c318e6dbea5cbc59ba292ead4ffe6
OlenaDovh <lothenshow@gmail.com>
2026-04-25 03:16:42 +0300

    fix: prepare project for production deployment

--- appointments/views/doctor.py
+++ appointments/views/doctor.py
@@ -2,14 +2,12 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from appointments.models import Appointment
 
-
 @login_required
 def doctor_appointment_list_view(request):
     doctor = request.user.doctor_profile
-
     appointments = Appointment.objects.filter(
         doctor=doctor
-    ).select_related("patient", "direction").order_by("-appointment_date")
+    ).select_related("user", "direction").order_by("-appointment_date")
 
     return render(request, "avelon_healthcare/appointments/pages/doctor_appointment_list.html", {
         "appointments": appointments
